189c10bde99a8b7e1a0c0d20b028248a03f2e835
Bailey <bailey.riezebos@gmail.com>
2020-04-05 15:14:39 +0200

    storage migrator & command

--- src/moda/plugin/module/nickname/Nickname.java
+++ src/moda/plugin/module/nickname/Nickname.java
@@ -4,11 +4,15 @@ import moda.plugin.moda.modules.Module;
 import moda.plugin.moda.utils.placeholders.ModaPlaceholderAPI;
 import moda.plugin.moda.utils.storage.DatabaseStorageHandler;
 import moda.plugin.moda.utils.storage.FileStorageHandler;
+import moda.plugin.moda.utils.storage.StorageMigrator;
 import moda.plugin.module.nickname.commands.NicknameCommand;
 import moda.plugin.module.nickname.storage.NicknameDatabaseStorageHandler;
 import moda.plugin.module.nickname.storage.NicknameFileStorageHandler;
 import moda.plugin.module.nickname.storage.NicknameStorageHandler;
+import moda.plugin.module.nickname.storage.NicknameStorageMigrator;
 import org.bukkit.configuration.InvalidConfigurationException;
+
+import java.io.IOException;
 
 public class Nickname extends Module<NicknameStorageHandler> {
 
@@ -33,8 +37,13 @@ public class Nickname extends Module<NicknameStorageHandler> {
     }
 
     @Override
-    public FileStorageHandler getFileStorageHandler() {
+    public FileStorageHandler getFileStorageHandler() throws IOException {
         return new NicknameFileStorageHandler(this);
+    }
+
+    @Override
+    public StorageMigrator<NicknameStorageHandler> getStorageMigrator() {
+        return new NicknameStorageMigrator();
     }
 
     @Override
@@ -44,7 +53,7 @@ public class Nickname extends Module<NicknameStorageHandler> {
         }
         ModaPlaceholderAPI.addPlaceholder("NICKNAME", player -> this.getStorage().getNickname(player));
         getLogger().info("Nickname module enabled.");
-        registerCommand(new NicknameCommand());
+        this.registerCommand(new NicknameCommand());
     }
 
     @Override
