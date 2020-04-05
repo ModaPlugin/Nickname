c9efdff27363a4b024aaa567e19ac4ccf599ee8f
Bailey <bailey.riezebos@gmail.com>
2020-04-06 01:42:49 +0200

    deprecate storage migrator

--- src/moda/plugin/module/nickname/storage/NicknameStorageMigrator.java
+++ src/moda/plugin/module/nickname/storage/NicknameStorageMigrator.java
@@ -1,14 +1,16 @@
 package moda.plugin.module.nickname.storage;
 
-import moda.plugin.moda.utils.storage.StorageMigrator;
+import moda.plugin.moda.module.storage.StorageMigrator;
 import org.bukkit.Bukkit;
 
 import java.util.UUID;
 
+@Deprecated
 public class NicknameStorageMigrator implements StorageMigrator<NicknameStorageHandler> {
+
 
     @Override
     public void migrate(NicknameStorageHandler from, NicknameStorageHandler to) throws Exception {
-        from.getAllPlayerData().get().forEach((uuid, nickname) -> to.setNickname(Bukkit.getOfflinePlayer(UUID.fromString(uuid)), nickname));
+//        from.getAllPlayerData().get().forEach((uuid, nickname) -> to.setNickname(Bukkit.getOfflinePlayer(UUID.fromString(uuid)), nickname));
     }
 }
