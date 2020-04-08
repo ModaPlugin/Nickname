b5454324fd71156c5ef9e5a1055495e9df6a3cc9
Bailey <bailey.riezebos@gmail.com>
2020-04-08 20:15:42 +0200

    fix reflection & blacklist checker

--- src/moda/plugin/module/nickname/utils/Colors.java
+++ src/moda/plugin/module/nickname/utils/Colors.java
@@ -5,6 +5,7 @@ import net.md_5.bungee.api.ChatColor;
 import org.bukkit.command.CommandSender;
 
 import java.lang.reflect.Field;
+import java.util.List;
 
 public class Colors {
 
@@ -13,11 +14,13 @@ public class Colors {
 
         for (ChatColor color : ChatColor.values()) {
             try {
-                Field field = ChatColor.class.getField("code");
+                Field field = ChatColor.class.getDeclaredField("code");
                 field.setAccessible(true);
                 char code = (char) field.get(color);
-                if (c.hasPermission("moda.plugin.module.nickname.set.color." + color.getName())) {
+                if (c.hasPermission("moda.plugin.module.nickname.set.color." + color.getName()) && !isBlacklisted(color)) {
                     s = s.replace("&" + code, ChatColor.COLOR_CHAR + "" + code);
+                } else {
+                    s = s.replace("&" + code, "");
                 }
             } catch (NoSuchFieldException | IllegalAccessException e) {
                 s = stripColors(s);
@@ -30,7 +33,16 @@ public class Colors {
 
     public static boolean isBlacklisted(ChatColor c) {
         Nickname n = Nickname.instance;
-        return n.getConfig().getStringList("blacklisted-colors").stream().anyMatch(s -> s.equalsIgnoreCase(c.name()));
+
+        boolean isBlacklisted = false;
+        List<String> blacklistedColors = n.getConfig().getStringList("blacklisted-colors");
+
+        if (blacklistedColors.stream().anyMatch(s -> s.trim().equalsIgnoreCase(c.name()))) {
+            isBlacklisted = true;
+        }
+
+        return isBlacklisted;
+//        return n.getConfig().getStringList("blacklisted-colors").stream().anyMatch(s -> s.equalsIgnoreCase(c.name()));
     }
 
     public static String stripColors(String s) {
