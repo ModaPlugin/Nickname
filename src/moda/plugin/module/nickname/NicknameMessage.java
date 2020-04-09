2c8ef7f4ab2935ef32922bf8a0812034c7f85c56
Bailey <bailey.riezebos@gmail.com>
2020-04-09 21:37:08 +0200

    add & update lang

--- src/moda/plugin/module/nickname/NicknameMessage.java
+++ src/moda/plugin/module/nickname/NicknameMessage.java
@@ -33,10 +33,6 @@ public enum NicknameMessage implements IMessage {
             // Level.INFO,
             "commands.nickname.self.reset",
             "&7You've reset &8(removed)&7 your nickname."),
-    COMMAND_NICKNAME_SELF_ERROR_UNKNOWN(
-            // Level.SEVERE,
-            "commands.nickname.self.error.unknown",
-            "&cAn unknown error has occurred while setting your nickname, please check the console"),
     COMMAND_NICKNAME_OTHER_UPDATE(
             // Level.INFO,
             "commands.nickname.other.update",
@@ -45,37 +41,49 @@ public enum NicknameMessage implements IMessage {
             // Level.INFO,
             "commands.nickname.other.reset",
             "&7You've reset &8(removed)&7 {TARGET}&7's nickname."),
-    COMMAND_NICKNAME_OTHER_ERROR_UNKNOWN(
-            // Level.SEVERE,
-            "commands.nickname.other.error.unknown",
-            "&cAn unknown error has occurred while setting &f{TARGET}&c's nickname, please check the console"),
-    COMMAND_NICKNAME_OTHER_ERROR_INVALID_PLAYER(
+    COMMAND_NICKNAME_OTHER_ERROR_INVALID_TARGET(
             // Level.SEVERE,
             "commands.nickname.other.error.player-not-found",
             "&cThe target &f{TARGET}&c was not found, is &7{TARGET}&c a valid minecraft user?"),
-    COMMAND_NICKNAME_WARN_COLOR(
+    COMMAND_NICKNAME_WARN_COLOR_BLACKLISTED(
             // Level.WARNING,
-            "commands.nickname.warn.color",
-            "&6You don't have permission to use colors in nicknames, the nickname will be set without colors"),
-    COMMAND_NICKNAME_WARN_FORMAT(
+            "commands.nickname.warn.color.blacklisted",
+            "&f{COLOR}&r&6 has been blacklisted by an admin, the nickname will be set without &f{COLOR}&r&6."),
+    COMMAND_NICKNAME_WARN_COLOR_CODE(
             // Level.WARNING,
-            "commands.nickname.warn.format",
-            "&6You don't have permission to use formatting in nicknames, the nickname will be set without formatting"),
+            "commands.nickname.warn.color.code",
+            "&6You need the permission &f{PERMISSION}&6 to use &f{COLOR}&r&6 in your nickname, the nickname will be set without &f{COLOR}&r&6."),
+    COMMAND_NICKNAME_WARN_COLOR_COLOR(
+            // Level.WARNING,
+            "commands.nickname.warn.color.color",
+            "&6You need the permission &f{PERMISSION}&6 to use colors in your nickname, the nickname will be set without colors"),
+    COMMAND_NICKNAME_WARN_COLOR_FORMAT(
+            // Level.WARNING,
+            "commands.nickname.warn.color.format",
+            "&6You need the permission &f{PERMISSION}&6 to use formatting in your nickname, the nickname will be set without formatting"),
     COMMAND_NICKNAME_WARN_TAKEN(
             // Level.WARNING,
             "commands.nickname.warn.taken",
             "&6The nickname &f{NICKNAME}&6 is already in use."),
-    COMMAND_NICKNAME_ERROR_NO_PERMISSION(
+    COMMAND_NICKNAME_ERROR_UNKNOWN(
             // Level.SEVERE,
-            "commands.nickname.error.no-permission",
-            "&cYou do not have permission to set nicknames."),
+            "commands.nickname.error.unknown",
+            "&cAn unknown error has occurred, please check the console"),
+    COMMAND_NICKNAME_ERROR_NO_PERMISSION_SELF(
+            // Level.SEVERE,
+            "commands.nickname.error.no-permission.self",
+            "&cYou need the permission &f{PERMISSION}&c to set your nickname."),
+    COMMAND_NICKNAME_ERROR_NO_PERMISSION_OTHER(
+            // Level.SEVERE,
+            "commands.nickname.error.no-permission.other",
+            "&cYou need the permission &f{PERMISSION}&c to set &f{TARGET}&c's nickname."),
     COMMAND_NICKNAME_ERROR_ILLEGAL_CHARACTER(
             // Level.SEVERE,
             "commands.nickname.error.illegal-character",
             "&cThe nickname you've provided contains illegal characters, nicknames cannot contain &f{ILLEGAL-CHAR}&c."),
-    COMMAND_NICKNAME_ERROR_SENDER_NOT_PLAYER(
+    COMMAND_NICKNAME_ERROR_INVALID_SENDER(
             // Level.SEVERE,
-            "commands.nickname.error.sender-not-player",
+            "commands.nickname.error.invalid-sender",
             "&cYou can only set your own nickname as a player."),
     COMMAND_NICKNAME_ERROR_TOO_MANY_ARGUMENTS(
             // Level.SEVERE,
